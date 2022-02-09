1b1c8c0f647eb3372c7f851f3e1f0b9eb0fcb1aa
Valerii Naida <vnayda@adobe.com>
2022-02-09 00:44:03 -0600

    1.0.3

--- Model/FetchProductData.php
+++ Model/FetchProductData.php
@@ -65,7 +65,7 @@ class FetchProductData implements FetchProductDataInterface
             '%s?instance=%s&product=%s',
             $config->getServiceUrl(),
             urlencode($config->getInstanceTag()),
-            $productSku
+            urlencode($productSku)
         );
 
         try {
